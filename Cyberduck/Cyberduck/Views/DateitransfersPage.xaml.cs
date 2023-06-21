4634407dc13681d7a229ffc2b6f676c3ebddc77b
Jan Wälti <jan.waelti98@hotmail.ch>
2023-06-21 11:12:12 +0200

    Added commandBar for "Dateitransfer" - page

--- Cyberduck/Cyberduck/Views/DateitransfersPage.xaml.cs
+++ Cyberduck/Cyberduck/Views/DateitransfersPage.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using Cyberduck.ViewModels;
-
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace Cyberduck.Views;
@@ -15,5 +15,44 @@ public sealed partial class DateitransfersPage : Page
     {
         ViewModel = App.GetService<DateitransfersViewModel>();
         InitializeComponent();
+    }
+
+
+
+    private ToggleMenuFlyoutItem selectedConnectionMenuItem;
+    private ToggleMenuFlyoutItem selectedBandwidthMenuItem;
+
+    //TODO: fix "automatic" selection
+    private void ConnectionMenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is ToggleMenuFlyoutItem selectedItem)
+        {
+            if (selectedConnectionMenuItem != null)
+            {
+                selectedConnectionMenuItem.IsChecked = false;
+            }
+
+            selectedItem.IsChecked = true;
+            selectedConnectionMenuItem = selectedItem;
+
+            selectedConnectionMenuItemText.Text = selectedItem.Text; // Update text in UI
+        }
+    }
+
+    //TODO: fix "automatic" selection
+    private void BandwithMenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is ToggleMenuFlyoutItem selectedItem)
+        {
+            if (selectedBandwidthMenuItem != null)
+            {
+                selectedBandwidthMenuItem.IsChecked = false;
+            }
+
+            selectedItem.IsChecked = true;
+            selectedBandwidthMenuItem = selectedItem;
+
+            selectedBandwidthMenuItemText.Text = selectedItem.Text; // Update text in UI
+        }
     }
 }
