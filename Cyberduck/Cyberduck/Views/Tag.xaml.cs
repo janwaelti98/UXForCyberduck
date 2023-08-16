4a2fac941bc6ed41c762b4a2cc2c8cc502b01fb7
Jan Wälti <jan.waelti98@hotmail.ch>
2023-08-16 16:17:08 +0200

    updated colors to match with dark / light theme

---
+++ Cyberduck/Cyberduck/Views/Tag.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace Cyberduck.Views;
+
+public sealed partial class Tag : UserControl
+{
+    public Tag()
+    {
+        InitializeComponent();
+    }
+}
